22a4c253480fe81579fd0ea06972e820527716ce
KhaledAmgad <khaled.mohamed98@eng-st.cu.edu.eg>
2019-11-03 18:14:17 +0200

    Get Contours & get Max Contour Area

--- humanSkin.py
+++ humanSkin.py
@@ -2,14 +2,48 @@ import cv2
 import numpy as np
 
 
+
+
+def getMaxContourArea(contours):
+    maxArea=0
+    maxContour=None
+    for contour in contours:
+        Xmin = np.min(contour[:,:,0])
+        Xmax = np.max(contour[:,:,0])
+        Ymin = np.min(contour[:,:,1])
+        Ymax = np.max(contour[:,:,1])
+        contourArea=(Xmax-Xmin)*(Ymax-Ymin)
+        if contourArea>maxArea:
+            maxArea=contourArea
+            maxContour=contour
+            
+    return maxArea,maxContour
+
+
+
+
+def getHand(frame):
+
+    
+    handBool=False
+    humanSkin=getHumanSkin(frame)
+    contours = cv2.findContours(humanSkin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+    if   len(contours)>0    :
+                maxContourArea,maxContour =getMaxContourArea(contours)
+                if maxContourArea > 2500:
+                    handBool=True
+                    x, y, w1, h1 = cv2.boundingRect(maxContour)
+                    humanSkin = humanSkin[y:y + h1, x:x + w1]
+        
+    return humanSkin,handBool
+
+
+
 def getHumanSkin(frame):
 
     img=np.copy(frame)
     
-    img =cv2.bilateralFilter(img,15,75,75)
-
-
-
+    img =cv2.bilateralFilter(img,15,75,75)  #blur without losing edges
 
     #converting from gbr to YCbCr color space
     img_YCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
@@ -17,15 +51,7 @@ def getHumanSkin(frame):
     YCrCb_mask = cv2.inRange(img_YCrCb, (0, 135, 85), (255,180,135)) 
     YCrCb_mask = cv2.morphologyEx(YCrCb_mask, cv2.MORPH_OPEN, np.ones((5,5), np.uint8))
     
-
-    
-
-
-    
-
-    humanSkin=np.copy(YCrCb_mask)
-
-    try:
+    '''try:
         positions = np.nonzero(humanSkin)
         top = positions[0].min()
         bottom = positions[0].max()
@@ -33,12 +59,9 @@ def getHumanSkin(frame):
         right = positions[1].max()
         humanSkin = humanSkin[top:top + bottom, left:left + right]
     except:
-        None
+        None'''
 
 
-
-    #show results
-    cv2.imshow("humanSkin",humanSkin)
     
-    return humanSkin
+    return YCrCb_mask
 
